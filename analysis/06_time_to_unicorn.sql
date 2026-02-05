a92a5ba9b8b79de138cbfee31aeb6bdaad7c3eae
Gamerx2478 <vishnuck29@gmail.com>
2026-02-06 00:13:51 +0530

    analysis: time to unicorn by industry

---
+++ analysis/06_time_to_unicorn.sql
@@ -0,0 +1,11 @@
+SELECT
+    i.industry_name,
+    ROUND(AVG(c.unicorn_year - c.founded_year), 2) AS avg_years_to_unicorn,
+    PERCENTILE_CONT(0.5)
+        WITHIN GROUP (ORDER BY (c.unicorn_year - c.founded_year)) AS median_years_to_unicorn,
+    COUNT(*) AS unicorn_count
+FROM companies c
+JOIN industries i USING (industry_id)
+GROUP BY i.industry_name
+HAVING COUNT(*) >= 3
+ORDER BY avg_years_to_unicorn;
