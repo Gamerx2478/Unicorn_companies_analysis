162b0e9c3f03e3d843f61c91a7887d050cf615cf
Gamerx2478 <vishnuck29@gmail.com>
2026-02-06 11:08:40 +0530

    Final Changes

--- ingestion/02_load_raw.sql
+++ ingestion/02_load_raw.sql
@@ -8,3 +8,7 @@ CREATE TABLE raw_unicorns (
     country TEXT,
     unicorn_year INT
 );
+COPY raw_unicorns
+FROM '/Users/adhithya/Library/CloudStorage/OneDrive-Personal/1. Projects/Unicorn_companies_analysis/Unicorn_companies_analysis/data/Unicorn_Startups.csv'
+DELIMITER ','
+CSV HEADER;
--- ingestion/03_transform_and_load.sql
+++ ingestion/03_transform_and_load.sql
@@ -9,3 +9,24 @@ INSERT INTO countries (country_name)
 SELECT DISTINCT country
 FROM raw_unicorns
 WHERE country IS NOT NULL;
+
+INSERT INTO companies (
+    company_name,
+    founded_year,
+    unicorn_year,
+    valuation_usd_bn,
+    industry_id,
+    country_id
+)
+SELECT
+    r.company,
+    r.founded_year,
+    r.unicorn_year,
+    CAST(REPLACE(REPLACE(r.valuation, '$', ''), ',', '') AS NUMERIC),
+    i.industry_id,
+    c.country_id
+FROM raw_unicorns r
+JOIN industries i
+    ON r.industry = i.industry_name
+JOIN countries c
+    ON r.country = c.country_name;
